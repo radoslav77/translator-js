0efd6c6b61f5f5d844866264ee000931ca5be549
Radoslav77 <rado7711@yahoo.co.uk>
2023-04-23 15:25:51 +0100

    01

---
+++ script.js
@@ -0,0 +1,27 @@
+let dictNewLang = {
+	'a': 'gre',	'b': 'rth',
+	'c': 'jpv',	'd': 'fgw',
+    'e': 'xqd','f': 'olp',
+    'g': 'ipq','h': 'hly',
+    'i': 'qbs','j': 'tcm',
+    'k': 'qux','l': 'ugi',
+    'm': 'kyxp',/*!!!!!!!*/
+    'n': 'wwj','o': 'cuv',
+    'p': 'nqk','r': 'wxh',
+    's': 'jtd','t': 'qis',
+    'u': 'itx','v': 'qpz',
+    'w': 'igx','x': 'fwj',
+    'y': 'gxig',/*!!!!!!!*/   
+    'z': 'sxn',   
+}
+
+const LeftInput = document.getElementById('textleft')
+const RightInput = document.getElementById('textright')
+
+
+
+const TransBtn = document.getElementById('trtanslate')
+
+TransBtn.addEventListener('click', ()=>{
+    console.log(LeftInput.value)
+})
